885560796c06b7d9f0c1ca881f12f9b0c148881d
naji-alhusami <najihussami@gmail.com>
2023-11-24 15:11:02 +0300

    added react-router-dom with Navbar and Home comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="text-2xl font-bold underline">
+//       <p>hello world!</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+
+// import ClipLoader from "react-spinners/ClipLoader";
+// import spinnerLoading from "./components/Images/spinnerLoading.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="text-2xl font-bold underline">
-      <p>hello world!</p>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<AboutContent />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/addplace" element={<AddPlace />} />
+        </Route>
+        <Route path="/signup" element={<Singup />} />
+        <Route path="/thanks" element={<Thanks />} />
+        <Route path="/iconicplaces" element={<IconicPlaces />} /> */}
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,5 @@
+function Navbar() {
+  return <h1>navbar</h1>;
+}
+
+export default Navbar;
