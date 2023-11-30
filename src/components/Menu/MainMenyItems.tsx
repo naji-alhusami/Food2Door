109403db87e23bc67e35bdcb8b50e2d30a59632b
naji-alhusami <najihussami@gmail.com>
2023-11-30 18:51:34 +0300

    working on animations for the items

--- src/components/Menu/MainMenyItems.tsx
+++ src/components/Menu/MainMenyItems.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 export type ItemData = {
   id: string;
   name: string;
@@ -10,16 +13,57 @@ type ItemDataProps = {
 };
 
 export default function MainMenuItemData({ items }: ItemDataProps) {
-  console.log(items);
+  //   const [showDetails, setShowDetails] = useState(false);
+  const [showDetailsItemId, setShowDetailsItemId] = useState<string | null>(
+    null
+  );
+
+  function showDetailsHandler(id: string) {
+    // Toggle showDetailsItemId only if it's the same item, otherwise, set it to the clicked item ID
+    setShowDetailsItemId((prevId) => (prevId === id ? null : id));
+    // setShowDetails(!showDetails);
+  }
 
   return (
-    <div className="flex flex-col flex-wrap justify-center items-center md:flex md:flex-row">
+    <div className="z-20 flex flex-col flex-wrap justify-center items-center md:flex md:flex-row">
       {items.map((item) => (
-        <div key={item.id} className="p-4 m-6 flex flex flex-col justify-center items-center border border-gray-200 gap-y-4">
-          <img src={item.image} alt={item.name} className="max-w-[15rem] max-h-[15rem]"/>
+        <div
+          key={item.id}
+          className="relative p-4 m-6 flex flex-col justify-center items-center border border-gray-200 gap-y-4 relative"
+        >
+          <img
+            src={item.image}
+            alt={item.name}
+            className="max-w-[15rem] max-h-[15rem]"
+          />
           <h2 className="text-xl font-bold">{item.name}</h2>
           <h2 className="text-[#ffc139] font-bold">{item.price} $</h2>
-          <button className="bg-[#ffc139] px-6 py-2  rounded-full">Show Details</button>
+          <button
+            onClick={() => showDetailsHandler(item.id)}
+            className="bg-[#ffc139] px-6 py-2 rounded-full hover:bg-[#ffae00] hover:text-white"
+          >
+            Show Details
+          </button>
+          {showDetailsItemId === item.id && (
+            <motion.div
+              initial={{ y: "100%" }}
+              animate={{ y: showDetailsItemId === item.id ? 0 : "100%" }}
+              transition={{ ease: "easeOut", duration: 10 }}
+              className="z-10 absolute bottom-0 left-0 w-full p-4 bg-white border-t border-gray-200 flex flex-col justify-center items-center"
+              style={{ visibility: showDetailsItemId ? "visible" : "hidden" }}
+            >
+              <h2 className="text-xl font-bold">{item.name}</h2>
+              <h2 className="text-[#ffc139] font-bold">{item.price}$ </h2>
+              <p>Details for {item.name}</p>
+
+              <button
+                onClick={() => showDetailsHandler(item.id)}
+                className="bg-[#ffc139] px-6 py-2 rounded-full hover:bg-[#ffae00] hover:text-white"
+              >
+                Add To Basket
+              </button>
+            </motion.div>
+          )}
         </div>
       ))}
     </div>
